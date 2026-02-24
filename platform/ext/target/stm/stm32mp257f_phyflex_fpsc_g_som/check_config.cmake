c4917c1acf576ee34a3be4ffef33fca940ad7b9e
Nicolas Raimundo <n.raimundo@phytec.fr>
2026-02-24 16:11:45 +0100

    platform: ext: target: stm: add stm32mp257f_phyflex_fpsc_g_som
    This platform is the PHYTEC SOM phyFLEX-STM32MP25x configured for
    CM33TDCID.
    
    Signed-off-by: Nicolas Raimundo <n.raimundo@phytec.fr>
    Signed-off-by: Christophe Parant <c.parant@phytec.fr>

---
+++ platform/ext/target/stm/stm32mp257f_phyflex_fpsc_g_som/check_config.cmake
@@ -0,0 +1,5 @@
+if (EXISTS ${STM_SOC_DIR}/check_config.cmake)
+	include(${STM_SOC_DIR}/check_config.cmake)
+endif()
+tfm_invalid_config(STM32_BOOT_DEV STREQUAL "ospi")
+tfm_invalid_config(TFM_PARTITION_PROTECTED_STORAGE)
